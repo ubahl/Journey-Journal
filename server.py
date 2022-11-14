a32b84c268b5f4049977c4512eff12577941f0f9
Uma Bahl <ubahl@scu.edu>
2022-11-14 10:39:55 -0500

    Last clean up

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ DATABASEURI = "postgresql://ank2177:8575@34.75.94.195/proj1part2"
 engine = create_engine(DATABASEURI)
 
 #
-# Sets up application to use cookies.
+# Sets up the application to use cookies.
 #
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
@@ -63,28 +63,8 @@ def teardown_request(exception):
 # @app.route is a decorator around index() that means:
 #   run index() whenever the user tries to access the "/" path using a GET request
 #
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-#
-# see for routing: https://flask.palletsprojects.com/en/2.0.x/quickstart/?highlight=routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
 @app.route('/')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
-
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: https://flask.palletsprojects.com/en/2.0.x/api/?highlight=incoming%20request%20data
-
-  """
-
   #
   # DATABASE QUERIES
   #
@@ -128,30 +108,7 @@ def index():
   cursor.close()
 
   #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #
-  #     # creates a <div> tag for each element in data
-  #     # will print:
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
+  # Variables passed to the Jinja template.
   #
   context = dict(data = journeys)
 
@@ -237,7 +194,6 @@ def insertNewJourney():
 
   # INPUT VERIFICATION (to prevent SQL injections / errors)
   # If it fails any, the query is not executed and the user is brought back to the home page.
-
   # Make sure ssn is a 9 digit number.
   if (len(ssn) != 9 or not re.search('^[0-9]*$', ssn)):
   	print("Invalid ssn")
@@ -287,7 +243,6 @@ def insertNewJourney():
   if not commuter_exists:
     g.conn.execute('INSERT INTO Commuter VALUES (%s, %s, %s, %s);', ssn, name, age, is_cs_student)
 
-  #ssn, identifier, train_id, start_station, end_station, rating, date = request.form['ssn', 'identifier', 'train_id', 'start_station', 'end_station', 'rating', 'date']
   g.conn.execute('INSERT INTO Journey VALUES (%s, %s, %s, %s, %s, %s, %s);', train_id, start_station, end_station, ssn, identifier, rating, date)
   return redirect('/')
 
